d6a78dc248266ad9c57062ee98fac59aa8f52a0e
David Silver <dsilver@candidatemetrics.com>
2013-04-08 20:22:04 -0700

    setting up Events

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Volunteers::Application.config.secret_token = '29906c650e94b1ec5ff1f153a05f99768d3dcccd5a2609796169b3da2cb85c3cb7e20ef003421e1072d98c5af837af579cf5d5c458d2b319e7e96703b86aaf34'
